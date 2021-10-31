6b22f1dada2ea9c1c95a4dcfa68ea642452e1326
Rebeca Lopes <rebeca.cruz@before.com.br>
2021-10-31 17:33:46 -0400

    feat: adiciona formtacao automatica de codigo

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2020: true,
+      node: true
+    },
+    extends: ["plugin:vue/essential", "plugin:prettier/recommended"],
+    parser: "vue-eslint-parser",
+    parserOptions: {
+      parser: "babel-eslint",
+      ecmaVersion: 11,
+      sourceType: "module"
+    },
+    plugins: ["vue"],
+    rules: {
+      "no-unused-vars": ["error", { "args": "none" }],
+    }
+  }
+  
