b7754a2c2362321fe5f74eed02f9d1421107cae8
Rebeca Lopes <rebeca.cruz@before.com.br>
2021-10-31 20:20:17 -0400

    feat: ajuste adiciona componentes

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container__larger">
+    <div>
+      <img alt="post" src="" />
+    </div>
+    <div>
+      <p>
+        The CSS Grid Layout Module offers a grid-based layout system, with rows
+        and columns, making it easier to design web pages without having to use
+        floats and positioning.
+      </p>
+    </div>
+    <span>Autor: Joaquim Silva Ler mais </span>
+  </div>
+</template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="footer">
+    <p>
+      <span class="footer__name">dev.blog </span>
+      <span class="footer__copyright">2021 copyright all rights reserved </span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  border-top: 1px solid #e8e8e8;
+  width: 70%;
+  text-align: center;
+  margin: 0 auto;
+
+  &__name {
+    @extend %label-inter-16-700;
+  }
+
+  &__copyright {
+    @extend %label-inter-12;
+  }
+}
+</style>
+
+
